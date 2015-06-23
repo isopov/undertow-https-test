e8499974fa69c7783daf5e4088b2e6f7b5b5fa77
sopov.ivan <sopov.ivan@otr.ru>
2015-06-23 11:36:06 +0300

    Gziphandler working sample with Jetty 9.2

--- src/main/java/com/sopovs/moradanen/JettyGzipTest.java
+++ src/main/java/com/sopovs/moradanen/JettyGzipTest.java
@@ -1,51 +1,57 @@
 package com.sopovs.moradanen;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.gzip.GzipHandler;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.servlets.gzip.GzipHandler;
 
 public class JettyGzipTest {
 
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
-		WebAppContext context = new WebAppContext();
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
 
-		File root = File.createTempFile("jetty-gzip", "test");
-		root.delete();
-		root.mkdirs();
-		File helloText = new File(root, "hello.txt");
-		try (FileWriter helloWriter = new FileWriter(helloText)) {
-			helloWriter.append("hello").flush();
-		}
+        GzipHandler gzipHandler = new GzipHandler();
+        gzipHandler.setMimeTypes("text/plain");
+        gzipHandler.setMinGzipSize(2048);
+        gzipHandler.setHandler(new TestHandler());
+        server.setHandler(gzipHandler);
 
-		char[] chars = new char[100000];
-		Arrays.fill(chars, 'F');
-		String largeContent = new String(chars);
-		File largeText = new File(root, "large.txt");
-		try (FileWriter largeWriter = new FileWriter(largeText)) {
-			largeWriter.append(largeContent).flush();
-		}
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.setConnectors(new Connector[] { connector });
 
-		context.setBaseResource(Resource.newResource(root.getCanonicalFile()));
+        server.start();
+        server.join();
+    }
 
-		GzipHandler gzipHandler = new GzipHandler();
-		gzipHandler.setIncludedMimeTypes("text/plain");
-		gzipHandler.setMinGzipSize(2048);
-		gzipHandler.setHandler(context);
-		server.setHandler(gzipHandler);
+    public static class TestHandler extends AbstractHandler {
 
-		ServerConnector connector = new ServerConnector(server);
-		connector.setPort(8080);
-		server.setConnectors(new Connector[] { connector });
+        @Override
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+                throws IOException, ServletException {
+            if ("/small".equals(target)) {
+                System.out.println(target);
+                response.setContentType("text/plain");
+                response.setContentLength("hello".length());
+                response.getWriter().println("hello");
+            } else {
+                response.setContentType("text/plain");
+                response.setContentLength(10000);
+                char[] chars = new char[100000];
+                Arrays.fill(chars, 'F');
+                String largeContent = new String(chars);
+                response.getWriter().println(largeContent);
+            }
+        }
 
-		server.start();
-		server.join();
-	}
+    }
 }
